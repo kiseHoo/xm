ba74b2acea9fb3f099c7f1ae85997f070ef21e66
kiseHoo <r.yt39158@gmail.com>
2025-11-24 09:11:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 # Owner & channels (set these in .env)
 OWNER_ID = int(os.getenv("OWNER_ID", "5738579437"))  # change if needed
 FORCE_CHANNEL_1 = os.getenv("FORCE_CHANNEL_1","@CuteBotUpdate")  # e.g. @YourChannel
-FORCE_CHANNEL_2 = os.getenv("FORCE_CHANNEL_2","@CuteDevlopers")  # e.g. @YourSecondChannel
+FORCE_CHANNEL_2 = os.getenv("FORCE_CHANNEL_2","@SkyRexo")  # e.g. @YourSecondChannel
 DUMP_CHANNEL = int(os.getenv("DUMP_CHANNEL", "-1003328559256"))    # e.g. -1001234567890
 
 bot = Client("xmaster_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
